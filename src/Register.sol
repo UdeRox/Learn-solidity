a4758dbcd91d571ac70e005adf270c3931862a29
udeshika <perera.n.u@gmail.com>
2023-08-20 06:02:04 +0800

    added a parem to the event

--- src/Register.sol
+++ src/Register.sol
@@ -6,18 +6,21 @@ contract Register {
     address[25] public users;
     uint256 public userSize;
 
-    event UserRegisterd (address indexed user, address indexed sender);
+    event UserRegisterd(
+        address indexed user,
+        uint256 indexed userId,
+        address sender
+    );
 
-    constructor(){
-        userSize=0;
-    } 
+    constructor() {
+        userSize = 0;
+    }
 
     function register(address _newUser) public {
         require(userSize < 25, "Only 25 Users can register!");
-        users[userSize]= _newUser;
-        userSize++;
+        users[userSize] = _newUser;
+        uint256 userId = userSize++;
         //emiting User Registerd event.
-        emit UserRegisterd(_newUser, msg.sender);
+        emit UserRegisterd(_newUser, userId, msg.sender);
     }
-
 }
